631d1eaa46bc454ae718107c9769dd068167b053
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 21:52:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
 ];
 
 function addElementToBeginningOfArray(array, element) {
-
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
