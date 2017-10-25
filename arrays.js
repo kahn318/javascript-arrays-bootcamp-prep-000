65d9105591409ab3c48473827612ce5015f8ba8b
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 22:02:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,5 +40,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeELementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
+  array.slice(0, array.length - 1);
 }
