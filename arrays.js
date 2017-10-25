8721bd5defdf47421eaed963781d03481fcbc9a4
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 21:57:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,7 @@ function accessElementInArray(array, index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift();
 }
+
+function removeElementFromBeginningOfArray(array) {
+  
+}
